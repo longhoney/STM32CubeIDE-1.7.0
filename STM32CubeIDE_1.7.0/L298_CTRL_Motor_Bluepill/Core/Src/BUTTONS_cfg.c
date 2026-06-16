696e9ca82eac9251209d1ae9c834fe77d4ea2c57
longhoney <long.711.sg@gmail.com>
2026-06-17 06:25:58 +0700

    Tam dung I2C, chuyen sang Bluepill PWM control DC motor

---
+++ STM32CubeIDE_1.7.0/L298_CTRL_Motor_Bluepill/Core/Src/BUTTONS_cfg.c
@@ -0,0 +1,17 @@
+/*
+ * BUTTONS_cfg.c
+ *
+ *  Created on: Jun 17, 2026
+ *      Author: longgg
+ */
+
+const BTN_CfgType BTN_CfgParam[BTN_UNITS] =
+{
+    // Button 1 Configurations
+    {
+        GPIOA,
+    GPIO_PIN_6,
+    GPIO_NOPULL,
+    BTNS_FILTER_ORDER
+    }
+};
